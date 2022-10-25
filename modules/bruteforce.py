85ed79382922d9b10d7c106c07fb3fafe7b02163
Jacobgarm <jacobgarm@hotmail.dk>
2022-10-25 09:56:32 +0200

    Fix bruteforce

--- modules/bruteforce.py
+++ modules/bruteforce.py
@@ -1,15 +1,15 @@
 from graph import Network
 
-def powerset(base_set: set) -> set[set]:
+def powerset(base_set: set) -> list[set]:
     """
     Calculates the power set of a set recursively 
     """
     if base_set == set():
-        return {set()}
+        return [set()]
     copied_set = base_set.copy()
     element = copied_set.pop()
     sub_powerset = powerset(copied_set)
-    return sub_powerset | {subset | {element} for subset in sub_powerset}
+    return sub_powerset + [subset | {element} for subset in sub_powerset]
 
 def brute_force_max_flow(network: Network) -> int:
     """
@@ -19,7 +19,7 @@ def brute_force_max_flow(network: Network) -> int:
     s = network.source
     t = network.sink
 
-    source_partitions = {{s} | subset for subset in powerset(network.vertices - {s, t})}
+    source_partitions = [{s} | subset for subset in powerset(network.vertices - {s, t})]
 
     min_cut_capacity = 999999999999999999999999999999999999999
 
@@ -31,23 +31,16 @@ def brute_force_max_flow(network: Network) -> int:
                 cut_capacity += network.get_capacity((v,u))
         if cut_capacity < min_cut_capacity:
             min_cut_capacity = cut_capacity
-    
     return min_cut_capacity
 
 net = Network()
 
-v = net.vertices
-e = net.edges
-c = {(net.source, 2): 3, (2, net.sink): 4, (net.source , 3): 4, (3, net.sink): 2}
 net.source = 1
 net.sink = 4
 
 net.add_verts([1, 2, 3, 4])
 net.add_edges([(net.source, 2), (2, net.sink), (net.source , 3), (3, net.sink)])
 
-brute_force_max_flow(net)
+net.capacity = {(net.source, 2): 3, (2, net.sink): 4, (net.source , 3): 4, (3, net.sink): 2}
 
-
-
-
-
+print(brute_force_max_flow(net))
