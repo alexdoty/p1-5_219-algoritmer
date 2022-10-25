f78e65317d4c0dae05b7bbbae27b22e16a517b69
alexdoty <adough22@student.aau.dk>
2022-10-25 11:31:07 +0200

    Add Ford-Fulkerson algorithm to modules

---
+++ modules/ford_fulkerson.py
@@ -0,0 +1,27 @@
+# Ford-Fulkerson Algorithm
+# ------------------------ 
+# Takes flow graph as input, so a graph G = (V, E) and a flow capacity c. We also need a start and a finish, so a source node s and "sink" node t
+# Returns flow with maximum capacity from s to t, which is a sequence
+# ------------------------
+# 1: 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+
+        
